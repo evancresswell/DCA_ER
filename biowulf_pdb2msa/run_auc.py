fixed ROC calculation indexing error by correctly aligne PDB sequences with match Pfam and ref-seq

biowulf_pdb2msa/run_auc.py
@@ -37,6 +37,8 @@ pdb_parser = Bio.PDB.PDBParser()
 
 from prody import *
 
+print('Done with initial import')
+
 
 create_new = False
 printing = True
@@ -85,11 +87,11 @@ print('Unzipping %s to %s' % (pdb_path, pdb_out_path))
 
 gunzip(pdb_path, pdb_out_path)
 
+print('Done unzipping pdb file')
 
 print(pdb_out_path)
 print(pdb_dir)
 pdb2msa_results = pdb2msa(pdb_out_path, pdb_dir, create_new=True)
-print(pdb2msa_results)
 
 if len(pdb2msa_results) > 1:
     fasta_file = pdb2msa_results[0]
@@ -103,43 +105,60 @@ print('\nPDB DF with associated Protein Families\n', prody_df.loc[:,  [column fo
 
 
 for ir, pdb2msa_row in enumerate(prody_df.iterrows()):
-    print('\n\nGetting msa with following pdb2msa entry:\n', pdb2msa_row)
+    print('\n\nGetting msa with following pdb2msa entry:\n', pdb2msa_row[1])
     try:
         dp_result =  data_processing_pdb2msa(data_path, prody_df.iloc[pdb2msa_row[0]], gap_seqs=0.2, gap_cols=0.2, prob_low=0.004,
-                               conserved_cols=0.8, printing=True, out_dir=processed_data_dir, pdb_dir=pdb_dir, letter_format=False,
-                               remove_cols=True, create_new=True, n_cpu=min(2, n_cpus))
+                           conserved_cols=0.8, printing=True, out_dir=processed_data_dir, pdb_dir=pdb_dir, letter_format=False,
+                           remove_cols=True, create_new=True, n_cpu=min(2, n_cpus))
         if dp_result is not None:
-            [s0, removed_cols, s_index, tpdb] = dp_result
+            [s0, removed_cols, s_index, tpdb, pdb_s_index] = dp_result
             break
         else:
             continue
     except Exception as e:
         print('row %d got exception: ' % ir , e)
         print('moving on.. ')
-        pass
+        continue
 pfam_id = pdb2msa_row[1]['Pfam']
 pdb_id = pdb2msa_row[1]['PDB ID']
+
+if removing_cols:
+    s0 = np.load("%s/%s_%s_preproc_msa.npy" % (processed_data_dir, pfam_id, pdb_id))
+    s_index = np.load("%s/%s_%s_preproc_sindex.npy" % (processed_data_dir, pfam_id, pdb_id))
+    pdb_s_index = np.load("%s/%s_%s_preproc_pdb_sindex.npy" % (processed_data_dir, pfam_id, pdb_id))
+    removed_cols = np.load("%s/%s_%s_removed_cols.npy" % (processed_data_dir, pfam_id, pdb_id))
+    ref_seq = np.load("%s/%s_%s_preproc_refseq.npy" % (processed_data_dir, pfam_id, pdb_id))
+else: 
+    s0 = np.load("%s/%s_%s_allCols_msa.npy" % (processed_data_dir, pfam_id, pdb_id))
+    s_index = np.load("%s/%s_%s_allCols_sindex.npy" % (processed_data_dir, pfam_id, pdb_id)) 
+    pdb_s_index = np.load("%s/%s_%s_allCols_pdb_sindex.npy" % (processed_data_dir, pfam_id, pdb_id)) 
+    removed_cols = np.load("%s/%s_%s_removed_cols.npy" % (processed_data_dir, pfam_id, pdb_id))
+    ref_seq = np.load("%s/%s_%s_allCols_refseq.npy" % (processed_data_dir, pfam_id, pdb_id))
+
+
 
 print('Done found correct prody_df row for contact predictions ... NOTE\n this step should be saved during data_processing!!!...')
 # ---------------------------------------------------------------------------- #
 
+print('\n\n...Done data_processing\n\n')
 
 
 
 di = np.load("%s/%s_%s_ER_di.npy" % (out_dir, pdb_id, pfam_id))
 
-ct, ct_full, n_amino_full, poly_seq_curated, poly_seq_range, poly_seq, pp_ca_coords_curated, pp_ca_coords_full_range =  \
-tools.contact_map_pdb2msa(pdb2msa_row[1], pdb_out_path, removed_cols, pdb_out_dir=pdb_dir, printing=True)
+ct, ct_full = tools.contact_map_pdb2msa_new(pdb2msa_row[1], pdb_out_path, removed_cols, pdb_s_index, pdb_out_dir=pdb_dir, printing=True)
 
 #print("Direct Information from Expectation reflection:\n",di)
-print('ER DI shape: ' , di.shape)
-print(removed_cols)
-if not removing_cols:
-    er_di = np.delete(di, removed_cols,0)
-    er_di = np.delete(er_di, removed_cols,1)
-else:
-    er_di = di
+print('s_index (%d): ' %len(s_index), s_index)
+print('ER DI shape (before removing cols): ' , di.shape)
+er_di = di
+#if not removing_cols:
+#    er_di = np.delete(di, removed_cols_range,0)
+#    er_di = np.delete(er_di, removed_cols_range,1)
+#else:
+#    er_di = di
 
+print('contact dimentions: ', ct.shape)
 print('Final ER DI shape (cols removed): ', er_di.shape)
 if remove_diagonals: 
     ER_di = no_diag(er_di, 4, s_index)
@@ -148,8 +167,8 @@ else:
 
 from ecc_tools import scores_matrix2dict
 print(s_index)
-ER_di_dict = scores_matrix2dict(ER_di, s_index, removed_cols, removing_cols=removing_cols)
-print(ER_di_dict[:20])
+#ER_di_dict = scores_matrix2dict(ER_di, s_index, removed_cols_range, removing_cols=removing_cols)
+#print(ER_di_dict[:20])
 
 
 from ecc_tools import roc_curve, roc_curve_new, precision_curve

data_processing.py
@@ -16,6 +16,7 @@ from Bio.PDB import *
 from scipy.spatial import distance_matrix
 from Bio import BiopythonWarning
 from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 from Bio.SubsMat.MatrixInfo import blosum62
 pdb_parser = Bio.PDB.PDBParser()
 
@@ -129,12 +130,12 @@ def pdb2msa(pdb_file, pdb_dir, create_new=True):
     if prody_df is None:
         print('PDB2MSA ERROR: No pdb matches found using prody.searchPfam for any of the chains/polypeptide sequences. !!')
         sys.exit(23)
-    print(prody_df.head())
+    #print(prody_df.head())
     print('sorting ProdyDataframe')
     
     prody_df = prody_df.sort_values(by='bitscore', ascending = False).sort_values(by='ind_evalue') # could be cond_evalue??
     prody_df = prody_df.reset_index(drop=True)
-    print(prody_df.head())
+    #print(prody_df.head())
 
     prody_df.to_csv('%s/%s_pdb_df.csv' % (pdb_dir, pdb_id))
 
@@ -150,17 +151,112 @@ def pdb2msa(pdb_file, pdb_dir, create_new=True):
         print('Error during fetPfamMSA: ',e)
         return [prody_df]
 
-def get_tpdb(s, prody_df):
+
+
+
+def get_tpdb_new(s, ali_start_indx, ali_end_indx, pfam_start_indx, pfam_end_indx, aligned_pdb_str):
+    # Requires DataFrame from data_processing.pdb2msa() function as input
+    from ecc_tools import hamming_distance
+    alignment_len =  ali_end_indx - ali_start_indx + 1
+    print('alignment length: ', alignment_len)
+    print('PDB sequence: (len %d)' % len(aligned_pdb_str), aligned_pdb_str)
+    #print(alignment_len, len(aligned_pdb_str), aligned_pdb_str)
+
+    min_ham = alignment_len
+    max_pair_score = 0
+    min_indx = -1
+    for i, seq in enumerate(s):
+        gap_seq = seq == '-'  # returns True/False for gaps/no gaps
+        subject = seq[~gap_seq]
+        seq_str = ''.join(subject).upper()
+        aligned_seq_str = seq_str[pfam_start_indx : pfam_end_indx+1]
+        if len(aligned_seq_str) != alignment_len:
+           continue
+        ham_dist = hamming_distance(aligned_pdb_str, aligned_seq_str)
+        alignments = pairwise2.align.globalxx(aligned_pdb_str, aligned_seq_str)
+        
+        if len(alignments) == 0:
+            continue
+        pair_score = alignments[0].score
+#         if ham_dist < min_ham:
+        if pair_score > max_pair_score:
+            min_ham = ham_dist
+            max_pair_score = pair_score
+            min_indx = i
+            print(format_alignment(*alignments[0]))
+            best_alignment = alignments[0]
+            print('match upgrade at' , i)
+            print('%d: hamm dist=%d, pairwise score=%f\n' % (i, ham_dist, pair_score))
+            print('lengths: ', len(aligned_pdb_str), len(aligned_seq_str))
+            print(best_alignment)
+
+
+            pair_score = alignments[0].score
+
+    gap_seq = s[min_indx] == '-'
+    print('best match is sequence %d with hamming distance of %d (length %d)' % (min_indx, min_ham, len(s[min_indx][~gap_seq])))
+    
+    
+    # get matching seq for both sequences (no gaps in either)
+    aligned_pdb_char_array = np.array([char for char in best_alignment.seqA])
+    aligned_ref_char_array = np.array([char for char in best_alignment.seqB])
+    
+    # get array of gaps for both sequences
+    seqA_gaps = aligned_pdb_char_array == '-'
+    seqB_gaps = aligned_ref_char_array == '-'
+    aligned_gaps = np.logical_or(seqA_gaps, seqB_gaps)
+
+    
+    # create index array for reference sequence so we know which msa columns associated with aligned arrays
+    pdb_count = 0
+    ref_count = 0
+    gap_ref_index = -1 * np.ones(len(aligned_ref_char_array), dtype=int)
+    gap_pdb_index = -1 * np.ones(len(aligned_pdb_char_array), dtype=int)
+    for i, char in enumerate(aligned_ref_char_array):
+        if char !='-':
+            gap_ref_index[i] = int(ref_count)
+            ref_count += 1
+        if aligned_pdb_char_array[i] !='-':
+            gap_pdb_index[i] = int(pdb_count)
+            pdb_count += 1            
+#     print(gap_ref_index)    
+    
+    
+    # get columns to remove (gap in PDB) in MSA
+    pdb_gap_cols_in_ref = gap_ref_index[seqA_gaps]
+    print(len(pdb_gap_cols_in_ref), pdb_gap_cols_in_ref)
+
+    # get s_index for mapping msa to pdb sequence.
+    pdb_s_index = gap_pdb_index[~aligned_gaps]
+    print(len(pdb_s_index), pdb_s_index)
+    
+    # Extract further infor for aligned seqs.
+    aligned_pdb_nogap = aligned_pdb_char_array[~aligned_gaps]
+    aligned_ref_nogap = aligned_ref_char_array[~aligned_gaps]
+    print('\n aligned PDB and ref seq:')
+    print(len(aligned_pdb_nogap),aligned_pdb_nogap)
+    print(len(aligned_ref_nogap),aligned_ref_nogap)
+    
+    # Trim By gaps in Ref seq (tbr). Then Trim By gaps in Pdb seq (tpb)
+    s_tbr = s[:, ~gap_seq]
+    print(s_tbr.shape)
+    s_tbr_tbp = np.delete(s_tbr, pdb_gap_cols_in_ref, axis=1)
+    print(s_tbr_tbp.shape)
+
+    # printed ref seq should be the same as the fully alinged, gapless pdb and ref seqs above.
+    print(len(s_tbr_tbp[min_indx]), s_tbr_tbp[min_indx])
+    
+
+
+    return min_indx, best_alignment, s_tbr_tbp, pdb_s_index
+
+
+
+
+def get_tpdb(s, ali_start_indx, ali_end_indx, pfam_start_indx, pfam_end_indx, aligned_pdb_str):
     from ecc_tools import hamming_distance
     # Requires DataFrame from data_processing.pdb2msa() function as input
-    ali_start_indx = int(prody_df['ali_start'])-1
-    ali_end_indx = int(prody_df['ali_end'])-1
-
-    pfam_start_indx = int(prody_df['start'])-1
-    pfam_end_indx = int(prody_df['end'])-1
-
     alignment_len =  ali_end_indx - ali_start_indx
-    aligned_pdb_str  = prody_df['PDB Sequence'][ali_start_indx:ali_end_indx]
     #print(alignment_len, len(aligned_pdb_str), aligned_pdb_str)
 
     min_ham = alignment_len
@@ -179,6 +275,8 @@ def get_tpdb(s, prody_df):
             min_indx = i
             continue                                                                                 
     return min_indx
+
+
 
 
 def query_pdb(msa, i):
@@ -707,6 +805,14 @@ def data_processing_pdb2msa(data_path, pdb_df,gap_seqs=0.2, gap_cols=0.2, prob_l
     pfam_id = pdb_df['Pfam']
     pdb_seq = pdb_df['PDB Sequence']
     pdb_id = pdb_df['PDB ID']
+    ali_start_indx = int(pdb_df['ali_start'])-1
+    ali_end_indx = int(pdb_df['ali_end'])-1
+    pfam_start_indx = int(pdb_df['hmm_start'])-1
+    pfam_end_indx = int(pdb_df['hmm_end'])-1
+
+    aligned_pdb_str  = pdb_df['PDB Sequence'][ali_start_indx:ali_end_indx+1]
+
+
     print('PDB ID: %s, Pfam ID: %s' % (pdb_id, pfam_id))
 
     np.random.seed(123456789)
@@ -738,28 +844,42 @@ def data_processing_pdb2msa(data_path, pdb_df,gap_seqs=0.2, gap_cols=0.2, prob_l
     # Using given MSA find best matching PDB structure from all available MSA sequences.
     
     if printing:
-        print("#\n\n--------------------- Find PDB Sequence in MSA ---------------#")
-        
+        print("\n\n#--------------------- Find PDB Sequence in MSA ---------------#")
+       
 
+       
     # Find PDB seq in MSA current
-    tpdb = get_tpdb(s, pdb_df) # requires prody.searchPfam DF from pdb2msa as input
-    if tpdb == -1:
-        print('No matching alignment to %s found in %s\n' % (pdb_id, pfam_id), pdb_df)
-        return None
+    tpdb, alignment, s, pdb_s_index = get_tpdb_new(s, ali_start_indx, ali_end_indx, pfam_start_indx, pfam_end_indx, aligned_pdb_str) # requires prody.searchPfam DF from pdb2msa as input
 
+    # follwoing no longer necessary with new version of get_tpdb..  get_tpdb_new does not retrun -1 AND get_tpdb_new trims ref sequence to correctly match aligned pdb sequence...
+    if 0:
+        if tpdb == -1:
+            print('No matching alignment to %s found in %s' % (pdb_id, pfam_id))
+            print("#--------------------------------------------------------------#\n\n")
+            return None
+
+    
+        gap_pdb = s[tpdb] == '-'  # returns True/False for gaps/no gaps
+        s = s[:, ~gap_pdb]        # removes gaps
 
     if printing:
+        print('MSA index %d matches PDB' % tpdb)
         print("#--------------------------------------------------------------#\n\n")
-    
-    
+
     if printing:
         print("#\n\n-------------------------Remove Gaps--------------------------#")
         print('Shape of s is : ', s.shape)
         print("s = \n", s)
+    
 
 
-    gap_pdb = s[tpdb] == '-'  # returns True/False for gaps/no gaps
-    s = s[:, ~gap_pdb]        # removes gaps
+ 
+    # remove gaps to allow alignment with PDB sequence..
+
+    # remove columns not in alignment range
+    in_range_indices = np.arange(ali_start_indx, ali_end_indx) 
+    print('in range indices: ', in_range_indices)
+
 
     s_index = np.arange(s.shape[1])
 
@@ -866,11 +986,13 @@ def data_processing_pdb2msa(data_path, pdb_df,gap_seqs=0.2, gap_cols=0.2, prob_l
     if remove_cols:
         s = np.delete(s, removed_cols, axis=1)
         s_index = np.delete(s_index, removed_cols)
+        pdb_s_index = np.delete(pdb_s_index, removed_cols)
 
     if printing and remove_cols:
         print("Removed Columns...")
         print("s now has shape: ", s.shape)
         print("s_index (length=%d) = \n" % s_index.shape[0], s_index)
+        print("pdb_s_index (length=%d) = \n" % pdb_s_index.shape[0], pdb_s_index)
         print("In data processing Ref Seq (shape=", s[tpdb].shape, "): \n", s[tpdb])
 
     # Convert S to number format (number representation of amino acids)
@@ -890,6 +1012,7 @@ def data_processing_pdb2msa(data_path, pdb_df,gap_seqs=0.2, gap_cols=0.2, prob_l
     if remove_cols:
         np.save("%s/%s_%s_preproc_msa.npy" % (out_dir, pfam_id, pdb_id), s)
         np.save("%s/%s_%s_preproc_sindex.npy" % (out_dir, pfam_id, pdb_id), s_index)
+        np.save("%s/%s_%s_preproc_pdb_sindex.npy" % (out_dir, pfam_id, pdb_id), s_index)
         np.save("%s/%s_%s_preproc_refseq.npy" % (out_dir, pfam_id, pdb_id), s[tpdb])
     else:
         np.save("%s/%s_%s_allCols_msa.npy" % (out_dir, pfam_id, pdb_id), s)
@@ -903,7 +1026,7 @@ def data_processing_pdb2msa(data_path, pdb_df,gap_seqs=0.2, gap_cols=0.2, prob_l
         print(s.shape)
         print("In Data Processing Final Reference Sequence (shape=", s[tpdb].shape, "): \n", s[tpdb])
 
-    return [s, removed_cols, s_index, tpdb]
+    return [s, removed_cols, s_index, tpdb, pdb_s_index]
 
 
 # =========================================================================================

ecc_tools.py
@@ -42,6 +42,47 @@ STANDARD_RESIDUES = {
         'LEU', 'LYS', 'MET', 'PHE', 'PRO',
         'SER', 'THR', 'TRP', 'TYR', 'VAL')
 }
+
+
+# Return the Hamming distance between string1 and string2.
+# string1 and string2 should be the same length.
+def hamming_distance(string1, string2): 
+    # Start with a distance of zero, and count up
+    distance = 0
+    # Loop over the indices of the string
+    L = len(string1)
+    for i in range(L):
+        # Add 1 to the distance if these two characters are not equal
+        if string1[i] != string2[i]:
+            distance += 1
+    # Return the final count of differences
+    return distance
+
+def window(fseq, window_size=5):
+    for i in range(len(fseq) - window_size + 1):
+        yield fseq[i:i+window_size]
+
+
+def filter_residues(residues, biomolecule='PROTEIN'):
+    """Filters the standared residues from others (e.g. hetatom residues).
+    Parameters
+    ----------
+        residues : list
+            A list of Biopython PDB structure residues objects.
+    Returns
+    -------
+        standard_residues : list
+            A lif of Biopython PDB structure standared residues (after hetro
+            atom residues are filtered).
+    """
+    biomolecule = biomolecule.strip().upper()
+    standard_residues = []
+    for res in residues:
+        if res.get_resname().strip() in STANDARD_RESIDUES[biomolecule]:
+            if not res.id[0].strip(): standard_residues.append(res) # filter out hetro residues
+    return standard_residues
+
+
 # -------------------------------------------------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------------------------------------------------- #
 # print("Direct Information from Expectation reflection:\n",di)
@@ -494,246 +535,6 @@ def write_FASTA(ref_seq, msa, pfam_id, number_form=True, processed=True, path='.
             ref_letters = msa_letters[ref_seq]
     else:
         msa_letters = msa
-        ref_array = np.asarray(ref_seq)
-        gap_ref = ref_array == '-'  # remove gaps from reference array
-        ref_letters = ref_array[~gap_ref]
-        msa_letters = np.delete(msa_letters, gap_ref, axis=0)
-    # First save reference sequence to FASTA file
-    ref_str = ''
-    ref_list = ref_letters.tolist()
-    ref_str = ref_str.join(ref_list)
-    if processed:
-        with open(ref_outfile, 'w') as fh:
-            fh.write('>{}\n{}\n'.format(pfam_id + ' | PP REFERENCE', ref_str))
-    else:
-        print('Writing Un-processed Reference sequence..\nOriginal Reference Sequence')
-        with open(ref_outfile, 'w') as fh:
-            fh.write('>{}\n{}\n'.format(pfam_id + ' | PP REFERENCE', ref_str))
-
-    # Next save MSA to FAST file
-
-    with open(msa_outfile, 'w') as fh:
-        for seq_num, seq in enumerate(msa_letters):
-            msa_list = seq.tolist()
-            msa_str = ''
-            msa_str = msa_str.join(msa_list)
-            fasta_header = pfam_id
-            fh.write('>{}\n{}\n'.format(fasta_header, msa_str))
-
-    # Return MSA and Reference FASTA file names
-    return msa_outfile, ref_outfile
-# -------------------------------------------------------------------------------------------------------------------- #
-
-# -------------------------------------------------------------------------------------------------------------------- #
-def split_and_shift_contact_pairs(list_of_contacts):
-    """
-    Separating contacting site pairs into two lists: One containing all first entries (xdata), and the other containing
-    all second entries (ydata). Note that, each site pair is shifted by 1 so as to make them ready for visualization
-    (since the sites have been counted starting from 0)
-
-    :param list_of_contacts:
-        list_of_contacts : list/tuple A list or tuple containing tuples of contacting site pairs.
-    :return:
-        xdata : list List containing shifted first entries of site pairs as obtained from list_of_contacts
-                parameter.
-        ydata : list List containing shifted second entries of site pairs as obtained from list_of_contacts parameter
-    """
-
-    xdata = list()
-    ydata = list()
-    for first, second in list_of_contacts:
-        xdata.append(first + 1)  # shift indexing by 1 for visualization (output)
-        ydata.append(second + 1)
-
-    return xdata, ydata
-
-
-def get_score_df(df_diff_full):
-    # pass dataframes generated in gen_method_column_df.py
-    # creates Dataframe for each method
-    # creates rgba list for 3 methods
-    printing = False
-    df_ER = df_diff_full.loc[df_diff_full['method'] == 'ER']
-    df_MF = df_diff_full.loc[df_diff_full['method'] == 'MF']
-    df_PLM = df_diff_full.loc[df_diff_full['method'] == 'PLM']
-
-    # print(df_MF.loc[df_MF['best_method']=='MF' ].loc[df_MF['AUC']<0.]['Score'])
-    if printing:
-        print(len(df_ER['Score']))
-        print(len(df_PLM['Score']))
-        print(len(df_MF['Score']))
-
-    common = df_ER.merge(df_MF, on='Pfam')
-    print(len(common))
-    df_ER = df_ER[df_ER.Pfam.isin(common.Pfam)].dropna()
-    df_PLM = df_PLM[df_PLM.Pfam.isin(common.Pfam)].dropna()
-    df_MF = df_MF[df_MF.Pfam.isin(common.Pfam)].dropna()
-
-    if printing:
-        print(len(df_ER['Score']))
-        print(len(df_PLM['Score']))
-        print(len(df_MF['Score']))
-    df_ER = df_ER.sort_values(by='Pfam')
-    df_MF = df_MF.sort_values(by='Pfam')
-    df_PLM = df_PLM.sort_values(by='Pfam')
-
-    if printing:
-        print(df_ER['Pfam'])
-        print(df_MF['Pfam'])
-        print(df_PLM['Pfam'])
-
-    df_winner = df_diff_full[df_diff_full.Pfam.isin(common.Pfam)].dropna()
-    df_winner = df_winner.loc[df_winner['best_method'] == df_winner['method']]
-    df_winner = df_winner.sort_values(by='Pfam')
-    scores = df_winner['Score'].values.tolist()
-    color_dict = {'ER': 'blue', 'PLM': 'green', 'MF': 'orange'}
-    colors = [color_dict[c] for c in df_ER['best_method'].values.tolist()]
-    print(len(scores), len(colors))
-    # cmap = colors.LinearSegmentedColormap.from_list('incr_alpha', [(0, (*colors.to_rgb(c),0)), (1, c)])
-    rgba_colors = np.zeros((len(scores), 4))
-    rgba_colors[:, 0:3] = [mpl_colors.to_rgb(c) for c in colors]
-    rgba_colors[:, 3] = scores
-    # print(rgba_colors)
-
-    return df_ER, df_MF, df_PLM, rgba_colors
-
-
-def gen_DI_matrix(DI):
-    n_seq = max([coupling[0][0] for coupling in DI])
-    di = np.zeros((n_var, n_var))
-    for coupling in DI:
-        di[coupling[0][0], coupling[0][1]] = coupling[1]
-        di[coupling[0][1], coupling[0][0]] = coupling[1]
-    return di
-
-
-def get_PFAM_PDB_map(pdb_id, pdb_out_dir='./'):
-    # Should be downloading pdb-pfam mapping for each protein on-demand from:
-    # 'ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/xml/%s/%s.xml.gz' % (pdb_id[1:3], pdb_id)
-    # and storing it here: xml_file = '%s/%s.xml' % (pdb_out_dir, pdb_id)
-
-    # create element tree object
-    tree = et.parse(xml_file)
-
-    # get root element
-    root = tree.getroot()
-
-    # create empty list for news items
-    newsitems = []
-
-    # iterate news items
-    for item in root.findall('./channel/item'):
-
-        # empty news dictionary
-        news = {}
-
-        # iterate child elements of item
-        for child in item:
-
-            # special checking for namespace object content:media
-            if child.tag == '{http://search.yahoo.com/mrss/}content':
-                news['media'] = child.attrib['url']
-            else:
-                news[child.tag] = child.text.encode('utf8')
-
-        # append news dictionary to news items list
-        newsitems.append(news)
-
-    # return news items list
-    return newsitems
-
-    """
-    from urllib.request import urlopen
-    from xml.etree.ElementTree import parse
-    
-    var_url = urlopen('https://www.ebi.ac.uk/pdbe/api/mappings/pfam/%s' % pdb_id)
-    xmldoc = parse(var_url)
-    
-    for item in xmldoc.iterfind('channel/item'):
-        title = item.findtext('title')
-        date = item.findtext('pubDate')
-        link = item.findtext('link')
-    
-        print(title)
-        print(date)
-        print(link)
-        print()
-    """
-
-
-
-# Return the Hamming distance between string1 and string2.
-# string1 and string2 should be the same length.
-def hamming_distance(string1, string2): 
-    # Start with a distance of zero, and count up
-    distance = 0
-    # Loop over the indices of the string
-    L = len(string1)
-    for i in range(L):
-        # Add 1 to the distance if these two characters are not equal
-        if string1[i] != string2[i]:
-            distance += 1
-    # Return the final count of differences
-    return distance
-
-def window(fseq, window_size=5):
-    for i in range(len(fseq) - window_size + 1):
-        yield fseq[i:i+window_size]
-
-
-def filter_residues(residues, biomolecule='PROTEIN'):
-    """Filters the standared residues from others (e.g. hetatom residues).
-
-    Parameters
-    ----------
-        residues : list
-            A list of Biopython PDB structure residues objects.
-
-    Returns
-    -------
-        standard_residues : list
-            A lif of Biopython PDB structure standared residues (after hetro
-            atom residues are filtered).
-    """
-    biomolecule = biomolecule.strip().upper()
-    standard_residues = []
-    for res in residues:
-        if res.get_resname().strip() in STANDARD_RESIDUES[biomolecule]:
-            if not res.id[0].strip(): standard_residues.append(res) # filter out hetro residues
-    return standard_residues
-
-def pdb2msa(pdb_file):
-    pdb_id = os.path.basename(pdb_file)[3:7]
-    print(pdb_id)
-    for record in SeqIO.parse(pdb_file, "pdb-seqres"):
-        print("Record id %s, chain %s" % (record.id, record.annotations["chain"]))
-        print(record.dbxrefs)
-    
-    pdb_model = pdb_parser.get_structure(str(pdb_id), pdb_file)[0]
-    for chain in pdb_model.get_chains():
-        ppb = PPBuilder().build_peptides(chain)
-        for i, pp in enumerate(ppb):
-            poly_seq = list()
-            for char in str(pp.get_sequence()):
-                poly_seq.append(char)
-            print('Chain %s polypeptide %d: ' % (chain.get_id(), i),''.join(poly_seq))
-   
-    # Use prody to find Pfam associated with PDB polypeptide sequence 
-    prody_search = searchPfam(''.join(poly_seq))
-
-    if 1:
-        # load the Pfam MSA from biowulf database
-        pass
-    else:
-        # Download the Pfam MSA with prody
-        for pfam_id in prody_search.keys():
-            msa_file = fetchPfamMSA(prody_search[pfam_id]['accession'])
-            print(msa_file)
-
-
-      
-
-def contact_map_new(pdb_id, pdb_range, removed_cols, queried_seq, mismatches, pdb_out_dir='./', printing=True):
     if printing:
         print('\n\n#-----------------------#\nGenerating Contact Map\n#----------------------------#\n')
 
@@ -880,8 +681,8 @@ def contact_map_pdb2msa(pdb_df, pdb_file, removed_cols, pdb_out_dir='./', printi
     pdb_id = pdb_df['PDB ID']
     # pdb_file = pdb_list.retrieve_pdb_file(pdb_id)
 
-    pdb_start = pdb_df['ali_start'] - 1
-    pdb_end = pdb_df['ali_end'] 
+    pdb_start = pdb_df['start'] - 1
+    pdb_end = pdb_df['end'] 
     pdb_chain = pdb_df['Chain']
     pdb_pp_index = pdb_df['Polypeptide Index']
 
@@ -914,21 +715,92 @@ def contact_map_pdb2msa(pdb_df, pdb_file, removed_cols, pdb_out_dir='./', printi
         print('\nChain ', chain, ':\n', ''.join(poly_seq))
         poly_seq_range = poly_seq[pdb_start:pdb_end]
         print( '\n',''.join(poly_seq_range), '\n')
+        print('poly_seq_range (%d)' % len(poly_seq_range))
+        print('pp seq coordinates (%d)' % len(pp_ca_coords_full))
 
     n_amino_full = len(pp_ca_coords_full)
 
     # Extract coordinates and sequence char in PDB-range\
     pp_ca_coords_full_range = pp_ca_coords_full[pdb_start:pdb_end]
+    print('pp seq coordinates in range (%d)' % len(pp_ca_coords_full_range))
 
     ct_full = distance_matrix(pp_ca_coords_full, pp_ca_coords_full)
-
-
-
-    poly_seq_curated = np.delete(poly_seq_range, removed_cols)
-    pp_ca_coords_curated = np.delete(pp_ca_coords_full_range, removed_cols, axis=0)
+    removed_cols_range = np.array([col-pdb_start for col in removed_cols if col<=pdb_end and col>=pdb_start])
+    print('curating removed columns in range..(%d -> %d)\n'% (len(removed_cols), len(removed_cols_range)))
+    print('poly_seq_range: ', len(poly_seq_range))
+    poly_seq_curated = np.delete(poly_seq_range, removed_cols_range)
+    pp_ca_coords_curated = np.delete(pp_ca_coords_full_range, removed_cols_range, axis=0)
     ct = distance_matrix(pp_ca_coords_curated, pp_ca_coords_curated)
 
-    return ct, ct_full, n_amino_full, poly_seq_curated, poly_seq_range, poly_seq, pp_ca_coords_curated, pp_ca_coords_full_range
+    return ct, ct_full, n_amino_full, poly_seq_curated, poly_seq_range, poly_seq, pp_ca_coords_curated, pp_ca_coords_full_range, removed_cols_range
+
+
+def contact_map_pdb2msa_new(pdb_df, pdb_file, removed_cols, pdb_s_index, pdb_out_dir='./', printing=True):
+    if printing:
+        print('\n\n#-----------------------#\nGenerating Contact Map\n#----------------------------#\n')
+
+    pdb_id = pdb_df['PDB ID']
+    # pdb_file = pdb_list.retrieve_pdb_file(pdb_id)
+
+    pdb_start = pdb_df['ali_start'] - 1
+    pdb_end = pdb_df['ali_end'] 
+    print(pdb_start, pdb_end)
+    shifted_pdb_s_index = [col+ pdb_start for col in pdb_s_index]
+    print('PDB sequence columns in di: ', shifted_pdb_s_index)
+    pdb_chain = pdb_df['Chain']
+    pdb_pp_index = pdb_df['Polypeptide Index']
+
+    pdb_model = pdb_parser.get_structure(str(pdb_id), pdb_file)[0]
+    found_pp_match = False
+    for chain in pdb_model.get_chains():
+        if chain.get_id() == pdb_chain:
+            pass
+        else:
+            continue
+        ppb = PPBuilder().build_peptides(chain)
+
+        # # PYDCA method for getting polypeptide sequence...
+        poly_seq_new = [res.get_resname().strip() for res in filter_residues(pdb_model[chain.get_id()].get_list())]
+        print('new poly seq list: ', ''.join(poly_seq_new))
+
+        # Get full list of CA coords from poly_seq
+        poly_seq = list()
+        pp_ca_coords_full = list()
+        for i, pp in enumerate(ppb):
+            if i == pdb_pp_index:
+                pass
+            else:
+                continue
+            for char in str(pp.get_sequence()):
+                poly_seq.append(char)
+            poly_seq_ca_atoms = pp.get_ca_list()
+            pp_ca_coords_full.extend([a.get_coord() for a in poly_seq_ca_atoms])
+
+        print('\nChain ', chain, ':\n', ''.join(poly_seq))
+        poly_seq_range = poly_seq[pdb_start:pdb_end]
+        print( '\n',''.join(poly_seq_range), '\n')
+        print('poly_seq_range (%d)' % len(poly_seq_range))
+        print('pp seq coordinates (%d)' % len(pp_ca_coords_full))
+
+        # NEW getting aligned poly seq and coords
+        aligned_poly_seq  = poly_seq[pdb_start:pdb_end]
+        aligned_poly_seq = [aligned_poly_seq[i] for i in pdb_s_index]
+        print('Aligned poly_seq: (len %d)' % len(aligned_poly_seq), aligned_poly_seq)
+        aligned_ca_coords = pp_ca_coords_full[pdb_start:pdb_end]
+        aligned_ca_coords = [aligned_ca_coords[i] for i in pdb_s_index]
+        print('Aligned pp ca coords len %d' % len(aligned_ca_coords))
+
+        
+    n_amino_full = len(pp_ca_coords_full)
+
+    # Extract coordinates and sequence char in PDB-range\
+
+    ct_full = distance_matrix(pp_ca_coords_full, pp_ca_coords_full)
+    
+    # NEW create ct using Aligned coordinates
+    ct = distance_matrix(aligned_ca_coords, aligned_ca_coords)
+
+    return ct, ct_full
 
 
 